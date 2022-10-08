d5944f29aa46c5c5e95c530eebbc8f2fadd05b55
sousamacedo18 <sousamacedo18@gmail.com>
2022-10-07 21:27:06 -0300

    Iniciando meu projeto de backend

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const usuarios=[
+    {nome:"Pedro",idade:20},
+    {nome:"João",idade:50},
+    {nome:"Maria",idade:19},
+    {nome:"Raimundo",idade:70}
+]
+app.get("/",(req,res)=>{
+   res.json({mensagem:"Hello Word"}) 
+})
+app.get("/usuarios",(req,res)=>{
+    res.send(usuarios) 
+ })
+module.exports = app;
