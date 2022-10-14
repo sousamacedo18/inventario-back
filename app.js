da1dd7dfe173ee562b111235192f79957c752be4
sousamacedo18 <sousamacedo18@gmail.com>
2022-10-13 21:37:09 -0300

    atualizando

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+
 const express = require("express");
 const app = express();
+const aluno=["CARLOS","ANTONIO","BISPO"];
+const alunos=[
+{nome:"CARLOS",endereco:"Rua Sta Cruz",idade:39},
+{nome:"ANTONIO",endereco:"Rua das Mangueiras",idade:18},
+{nome:"Bispo",endereco:"Rua 21 de Abril",idade:23}
+]
+const cadastro ={
+    nome:"CARLOS",
+    endereco:"Rua Sta Cruz",
+    idade:39,
+    cursos:["informática","pedreiro"],
+    conjuge:[
+        {nome:"Maria",idade:20,sexo:"F"},
+        {nome:"Raimundo",idade:25,sexo:"M"}
+    ]
+    
+}
 const usuarios=[
     {nome:"Pedro",idade:20},
     {nome:"João",idade:50},
@@ -7,9 +25,12 @@ const usuarios=[
     {nome:"Raimundo",idade:70}
 ]
 app.get("/",(req,res)=>{
-   res.json({mensagem:"Hello Word"}) 
+
+    let idade =String(cadastro.conjuge[1].idade);
+   res.send(idade);
 })
-app.get("/usuarios",(req,res)=>{
-    res.send(usuarios) 
+app.get("/usuarios/:id",(req,res)=>{
+    const id=req.params.id;
+    res.send(usuarios[2]) 
  })
 module.exports = app;
